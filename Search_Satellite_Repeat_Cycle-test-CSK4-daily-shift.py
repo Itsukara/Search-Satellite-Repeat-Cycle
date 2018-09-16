a8c63d896d84023bab8bd05908e580b855930419
Itsukara <iitt21-t@yahoo.co.jp>
2018-09-16 21:48:02 +0900

    Add sample to investigate orbit shift
    Add sample to investigate orbit shift in longitude direction each day

---
+++ Search_Satellite_Repeat_Cycle-test-CSK4-daily-shift.py
@@ -0,0 +1,21 @@
+import Search_Satellite_Repeat_Cycle as ssrc
+
+# INPUT DATA
+# Two Line Element
+TLE1 = '''COSMO-SKYMED 4          
+1 37216U 10060A   18258.17806865 -.00000011  00000-0  51372-5 0  9997
+2 37216  97.8902  80.4717 0001356  76.0322 284.1025 14.82152713425139'''
+
+TLEs = (TLE1,)
+
+# dates to search
+datestrs = ("2018-09-17 00:39:00",)
+
+# Search Condition
+# Invesigate orbit shift in longitude each day
+maxlat   = 1.0 # max error of latitude[deg]
+maxlong  = 10.0 # max error of longitude[deg]
+maxdays  =  14 # max days of search
+
+ssrc.searchRepeatCycles(TLEs, datestrs, maxlat, maxlong, maxdays)
+
