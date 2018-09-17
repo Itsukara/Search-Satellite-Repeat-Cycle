457fcfbb0de72e62928fc752fda3e938a270f368
Itsukara <iitt21-t@yahoo.co.jp>
2018-09-18 02:10:05 +0900

    Add more parameter to Cloud Function
    Add more parameter to Cloud Function.
    Change Repeat_Cycle_Cloud_Function-main.py as easy testable in local Flask environment.

--- Repeat_Cycle_Cloud_Function-main.py
+++ Repeat_Cycle_Cloud_Function-main.py
@@ -1,3 +1,39 @@
+from flask import Flask
+#from flask import request
+from flask import Response
+
+#app = Flask(__name__)
+
+### micro-utility for flask
+def p0():
+    global lines
+    lines = ""
+    
+def p(l):
+    global lines
+    lines += str(l) + "\n"
+
+def pn(l):
+    global lines
+    lines += str(l)
+
+def r():
+    return Response(lines, mimetype="text/plain")
+
+def get_args(request, names):
+    ns = names.split(",")
+    r  = []
+    for n in ns:
+        if n in request.args:
+            r.append(request.args[n])
+        elif n in request.form:
+            r.append(request.form[n])
+        else:
+            r.append("")
+    return r
+
+### body
+
 import ephem
 
 # Get Ephemeris Data
@@ -46,7 +82,7 @@ def jday2str(jday):
 # maxlong: max error of longitude[deg]
 # maxdays: max days of search
 import math
-def searchRepeatCycle(TLE, dt, maxlat, maxlong, maxdays):
+def searchRepeatCycle(TLE, dt, maxlat, maxlong, maxdays, maxlongKm):
     # Initial Date
     dt0 = dt
 
@@ -57,13 +93,11 @@ def searchRepeatCycle(TLE, dt, maxlat, maxlong, maxdays):
 
     # Search Repeat Cycle
     eps      = obperiod0 / 360 / 10
-    latlen   = 40009 # Circumference - meridional [Km]
-    longlen  = 40075 # Circumference - quatorial  [Km]
+    latKm   = 40009 # Circumference - meridional [Km]
+    longKm  = 40075 # Circumference - quatorial  [Km]
 
-    lines = ""
     dt = dt0
-#    print("                                         Lat(+N) diff[deg]   diff[Km] |   Long(+E)  diff[deg]   diff[Km]")
-    lines += "                                         Lat(+N) diff[deg]   diff[Km] |   Long(+E)  diff[deg]   diff[Km]\n"
+    p("                                        Lat(+N)  diff[deg]   diff[Km] |   Long(+E)  diff[deg]   diff[Km]")
     for d in range(int(maxdays * mmotion0)):
       (latitude, longitude, _, _, _) = getEphem(satellite, dt)
       difflat   = latitude  - latitude0
@@ -72,48 +106,62 @@ def searchRepeatCycle(TLE, dt, maxlat, maxlong, maxdays):
       if abs(difflat) < maxlat and abs(difflong) <maxlong:
         dtstr =jday2str(dt)
         days  = dt - dt0
-        difflatlen  = difflat  / 360 * latlen
-        difflonglen = difflong / 360 * longlen * math.cos(math.radians(latitude))
-#        print("[%s = %6.2f(days)] %10.4f %+10.4f %+10.4f | %10.4f %+10.4f %+10.4f" % (dtstr, days, latitude, difflat, difflatlen, longitude, difflong, difflonglen))
-        lines += "[%s = %6.2f(days)] %10.4f %+10.4f %+10.4f | %10.4f %+10.4f %+10.4f\n" % (dtstr, days, latitude, difflat, difflatlen, longitude, difflong, difflonglen)
+        difflatKm  = difflat  / 360 * latKm
+        difflongKm = difflong / 360 * longKm * math.cos(math.radians(latitude))
+        p("[%s = %6.2f(days)] %10.4f %+10.4f %+10.4f | %10.4f %+10.4f %+10.4f" % (dtstr, days, latitude, difflat, difflatKm, longitude, difflong, difflongKm))
+        if abs(difflongKm) > 0.001 and abs(difflongKm) < maxlongKm:
+            p("*** Found Repeat Cycle (Long diff[Km] < " + str(maxlongKm) + ")")
+            break
 
       # update dt
       dt = lat0date(satellite, dt + obperiod0, latitude0, eps)
-    
-    return lines
 
-from flask import Response
-def ephem_test(request):
-    TLE  = ""
-    date = ""
-    if request:
-        request_json = request.get_json()
-        if request.args:
-            if 'TLE' in request.args:
-                TLE  = request.args.get('TLE')
-            if 'date' in request.args:
-                date = request.args.get('date')
-        elif request_json:
-            if 'TLE' in request_json:
-                TLE  = request_json['TLE']
-            if 'date' in request_json:
-                date = request_json['date']
-        else:
-            if 'TLE' in request.form:
-                TLE  = request.form['TLE']
-            if 'date' in request.form:
-                date = request.form['date']
-    if not TLE:
-        TLE = '''COSMO-SKYMED 4          
+CSKTLE = '''COSMO-SKYMED 4          
 1 37216U 10060A   18258.17806865 -.00000011  00000-0  51372-5 0  9997
 2 37216  97.8902  80.4717 0001356  76.0322 284.1025 14.82152713425139'''
-    if date:
-        dt = ephem.Date(date)
-    else:
-        dt = ephem.now()
+
+#@app.route('/', methods=['GET', 'POST'])
+#def ephem_test():
+def ephem_test(request):
+    p0()
+
+    (TLE, date, maxlat, maxlong, maxdays, maxlongKm) = get_args(request, "TLE,date,maxlat,maxlong,maxdays,maxlongKm")
+    TLE = TLE or CSKTLE
+    TLE = TLE.replace("\r\n", "\n") if "\r\n" in TLE else TLE
+    dt  = ephem.Date(date) if date else ephem.now()
     
-    maxlat   = 1.0 # max error of latitude[deg]
-    maxlong  = 1.0 # max error of longitude[deg]
-    maxdays  = 180 # max days of search
-    text = TLE + "\n\n" + searchRepeatCycle(TLE, dt, maxlat, maxlong, maxdays)
-    return Response(text, mimetype='text/plain')
+    # Search Condition
+    maxlat    = float(maxlat)  if maxlat  else 1.0 # max error of latitude[deg]
+    maxlong   = float(maxlong) if maxlong else 1.0 # max error of longitude[deg]
+    maxdays   = float(maxdays) if maxdays else 120 # max days of search
+    maxlongKm = float(maxlongKm) if maxlongKm else 10.0 # max Long diff[Km]
+
+    '''comment
+    pn("TLE  ="); p(TLE)
+    pn("date ="); p(date)
+    pn("dt   ="); p(dt)
+    pn("maxlat  ="); p(maxlat)
+    pn("maxlong ="); p(maxlong)
+    pn("maxdays ="); p(maxdays)
+    pn("maxlongKm ="); p(maxlongKm)
+    return r()
+   '''
+
+    p(TLE + "\n")
+    searchRepeatCycle(TLE, dt, maxlat, maxlong, maxdays, maxlongKm)
+    return r()
+
+'''
+@app.route('/hello', methods=['GET', 'POST'])
+def hello_world():
+    p0()
+    pn("request.method="); p(request.method)
+    pn("request.args  ="); p(request.args)
+    pn("request.form  ="); p(request.form)
+ 
+    (TLE, date) = get_args(request, "TLE,date")
+    pn("TLE ="); p(TLE)
+    pn("date="); p(date)
+    
+    return r()
+'''
